726e481c33651883a128cff64eb71dc084281d67
Oscah01 <pipkillerinfo@gmail.com>
2023-03-02 18:26:25 +0300

    setup components

---
+++ src/pages/Books.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from '../component/Book';
+import Form from '../component/Form';
+
+const Books = () => (
+  <>
+    <Book title="Dad why are we poor" author="Oscar Deus" />
+    <Form />
+  </>
+);
+
+export default Books;
