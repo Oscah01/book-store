d8b529ca010571269f889a3f18d5efaa5b5b1004
Oscah01 <pipkillerinfo@gmail.com>
2023-03-07 00:46:15 +0300

    add redux-toolkit with reduce and actions

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// initial state
+const initialState = {
+  categories: [],
+  status: 'Under development',
+};
+
+// create slice
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state) => ({
+      ...state,
+      status: 'Under construction',
+    }),
+  },
+});
+
+// export actions
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
