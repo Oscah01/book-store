d01202fec238c93864c4486a8c9ca65dfebd3acc
Oscah01 <pipkillerinfo@gmail.com>
2023-03-09 16:20:18 +0300

    fetched api

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import RemoveBtn from './buttons/RemoveBtn';
-import { removeBook } from '../redux/books/bookSlice';
+import { booksActions, deleteBook } from '../redux/books/bookSlice';
 
 const Book = ({ item }) => {
+  const { title, author } = item;
   const dispatch = useDispatch();
-  const handleDelete = (id) => {
-    dispatch(removeBook({ id }));
+
+  const removeBookHandler = () => {
+    dispatch(booksActions.removeBook(item.item_id));
+
+    dispatch(deleteBook(item.item_id));
   };
 
   return (
     <>
       <div>
-        <section>
-          <span>{item.author}</span>
-          <span>{item.title}</span>
-          <RemoveBtn onClick={() => handleDelete(item.item_id)} />
-        </section>
+        <h2>{title}</h2>
+        <span>
+          By
+          {author}
+        </span>
+        <button type="button" onClick={removeBookHandler}>
+          Remove
+        </button>
       </div>
     </>
   );
 };
 
+Book.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 export default Book;
-Book.propTypes = { item: PropTypes.objectOf.isRequired };
--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,41 +1,59 @@
 import { useDispatch } from 'react-redux';
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/bookSlice';
-import AddBtn from './buttons/AddBtn';
+import { booksActions, postBook } from '../redux/books/bookSlice';
 
 const Form = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    const itemId = `item${uuidv4()}`;
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const authorChangeHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(addBook({ item_id: itemId, title, author }));
+
+    if (!title.trim() || !author.trim()) return;
+
+    const bookData = {
+      item_id: uuidv4(),
+      title,
+      author,
+      category: 'Fiction',
+    };
+
+    dispatch(booksActions.addBook(bookData));
+    dispatch(postBook(bookData));
+    setTitle('');
+    setAuthor('');
   };
 
   return (
-    <div>
-      <form>
-        <div>
-          <input
-            type="text"
-            placeholder="Book Title"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="author"
-            placeholder="Author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-        </div>
-        <AddBtn handleSubmit={handleSubmit} />
+    <div className="form-wrapper row">
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          aria-label="Book title input"
+          placeholder="Book Title"
+          onChange={titleChangeHandler}
+        />
+        <input
+          type="text"
+          name="author"
+          value={author}
+          aria-label="Book author input"
+          placeholder="Author"
+          onChange={authorChangeHandler}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
