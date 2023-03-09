4eba4a54154b70e68e5e77ad17353527e17900c0
Oscah01 <pipkillerinfo@gmail.com>
2023-03-09 16:23:26 +0300

    fetched api

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <nav>
     <div className="header">
-      <h1 className="title">Book Store </h1>
+      <h1 className="title">Book Stores </h1>
 
       <ul>
         <li><Link to="/">Books</Link></li>
