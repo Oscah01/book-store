02dde31e80f76ae9a2774e92e5f8137a4d3621ce
Oscah01 <pipkillerinfo@gmail.com>
2023-03-09 17:01:43 +0300

    update use effect

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from '../component/Book';
 import Form from '../component/Form';
+import { getBooks } from '../redux/books/bookSlice';
 
 const Books = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
   const booklists = useSelector((state) => state.books.books);
 
   return (
